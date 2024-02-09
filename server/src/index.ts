71cec8802500af846578422aec6740ddd0ba8d5e
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-02-09 19:44:51 +0200

    added simple express server
    created /healthz endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+const port = 8080 ?? process.env.PORT;
+
+app.get("/healthz", (req, res) => {
+  res.sendStatus(200);
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
