f010d901fbb1328d0a62b3fb3ae70ae67da7d11a
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-02-09 22:38:46 +0200

    added locationResultSchema with zod
    locationResultSchema is created to validate data from google maps api, it also allows runtime typechecking for incoming data

---
+++ server/src/schemas/locationResultSchema.ts
@@ -0,0 +1,75 @@
+import {z} from "zod";
+
+export const locationSchema = z.object({
+  lat: z.number(),
+  lng: z.number(),
+})
+
+const placeGeometrySchema = z.object({
+  location: locationSchema,
+  viewport: z.object({
+    northeast: locationSchema,
+    southwest: locationSchema
+  }),
+});
+
+const placeOpeningHoursPeriodSchema = z.object({
+  close: z.object({
+    day: z.number(),
+    time: z.string(),
+  }),
+  open: z.object({
+    day: z.number(),
+    time: z.string(),
+  }),
+});
+
+const placeOpeningHoursSchema = z.object({
+  open_now: z.boolean(),
+  periods: z.array(placeOpeningHoursPeriodSchema).optional(),
+  weekday_text: z.array(z.string()).optional(),
+});
+
+const placePhotoSchema = z.object({
+  height: z.number(),
+  html_attributions: z.array(z.string()),
+  photo_reference: z.string(),
+  width: z.number(),
+});
+
+const placePlusCodeSchema = z.object({
+  compound_code: z.string(),
+  global_code: z.string(),
+});
+
+export const placeSchema = z.object({
+  business_status: z.string().optional(),
+  formatted_address: z.string().optional(),
+  geometry: placeGeometrySchema,
+  icon: z.string(),
+  name: z.string(),
+  opening_hours: placeOpeningHoursSchema.optional(),
+  photos: z.array(placePhotoSchema).optional(),
+  place_id: z.string(),
+  plus_code: placePlusCodeSchema.optional(),
+  rating: z.number().optional(),
+  reference: z.string(),
+  types: z.array(z.string()),
+  user_ratings_total: z.number().optional(),
+});
+
+export const placesNearbySearchResponseSchema = z.object({
+  html_attributions: z.array(z.string()).optional(),
+  next_page_token: z.string().optional(),
+  results: z.array(placeSchema),
+  status: z.string(),
+});
+
+export default placesNearbySearchResponseSchema;
+
+
+export type PlacesNearbySearchResponse = z.infer<typeof placesNearbySearchResponseSchema>;
+
+export type Place = z.infer<typeof placeSchema>;
+
+export type Location = z.infer<typeof locationSchema>;
