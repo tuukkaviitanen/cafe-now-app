cc1dc641de5e5d6791dc55fef373a596242fdbb9
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-02-09 21:27:02 +0200

    added axios as dependency

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,9 +1,18 @@
 
-export const port = Number(process.env.PORT) ?? 8080;
+export const port = Number(process.env.PORT) | 8080;
 
 export const googleApiKey = process.env.GOOGLE_API_KEY;
 
+export const nodeEnv = process.env.NODE_ENV;
+
 if (!googleApiKey){
-  console.error("Google API Key not set. Program will abort.")
+  console.error("GOOGLE_API_KEY not set. Program will abort.")
   process.abort();
 }
+
+if (!nodeEnv){
+  console.error("NODE_ENV not set. Program will abort.")
+  process.abort();
+}
+
+console.log(`Program is running in ${nodeEnv} mode`)
