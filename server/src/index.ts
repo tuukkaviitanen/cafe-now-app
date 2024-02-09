cab3401c13562ec7a1813c5dcc1fc899ef80296f
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-02-09 22:42:39 +0200

    added endpoint for locations
    /locations endpoint uses locationService to fetch the mock data and returns the array of places to user

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import * as config from "./config"
+import locationService from "./services/locationService";
 
 const app = express();
 const port = config.port;
+
+app.get("/locations", async (req, res) => {
+  res.send(await locationService.getNearbyCafes())
+})
 
 app.get("/healthz", (req, res) => {
   res.sendStatus(200);
