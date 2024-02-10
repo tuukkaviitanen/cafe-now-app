cc5dfd5a725974f4276254861052dd211999400c
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-02-10 08:58:01 +0200

    added middleware.ts and errorHandler middleware
    errorHandler handles zodErrors and unexpected errors

---
+++ server/src/utils/middleware.ts
@@ -0,0 +1,17 @@
+import type { ErrorRequestHandler } from "express";
+import { ZodError } from "zod";
+import { fromZodError } from "zod-validation-error";
+
+export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+
+  console.error({error: error.message});
+
+  if(error instanceof ZodError){
+    const {message} = fromZodError(error)
+    return res.status(400).json({error: message})
+  }
+
+  return res.status(500).json({error: "unexpected error occured"})
+}
+
+
