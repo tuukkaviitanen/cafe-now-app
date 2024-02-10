70f005acb51f888258b4f1b4dd3539c430b48759
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-02-10 09:02:06 +0200

    added MOCK_API_URL env variable to config

--- server/src/config.ts
+++ server/src/config.ts
@@ -5,6 +5,8 @@ export const googleApiKey = process.env.GOOGLE_API_KEY;
 
 export const nodeEnv = process.env.NODE_ENV;
 
+export const mockApiUrl = process.env.MOCK_API_URL;
+
 if (!googleApiKey){
   console.error("GOOGLE_API_KEY not set. Program will abort.")
   process.abort();
@@ -13,6 +15,10 @@ if (!googleApiKey){
 if (!nodeEnv){
   console.error("NODE_ENV not set. Program will abort.")
   process.abort();
+}
+
+if (nodeEnv !== "production" && !mockApiUrl){
+  console.error("Program is set to run a development build, but MOCK_API_URL is not provided. Program will abort.")
 }
 
 console.log(`Program is running in ${nodeEnv} mode`)
