5e1e82df7cc788967ba2bfd8e9beec438f8a26f9
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-02-18 19:58:33 +0200

    removed rate-limiter and added helmet back
    rate limiter seems to be the one causing problems

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,19 +4,11 @@ import 'express-async-errors';
 import locationService from './services/locationService';
 import { placesNearbySearchRequestSchema } from './schemas/placesNearbySearchRequestSchema';
 import { errorHandler } from './utils/middleware';
-import rateLimit from 'express-rate-limit';
-import { nodeEnv } from './utils/environment';
+import helmet from 'helmet';
 import { ServiceError } from './utils/errors';
 const app = express();
 
-if (nodeEnv === 'production') {
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
-      max: 15, // Max number of requests allowed in that time frame
-    }),
-  );
-}
+app.use(helmet());
 
 app.use(express.json());
 
