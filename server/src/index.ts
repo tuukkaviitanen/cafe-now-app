fe8cb2ef26738ebeb5104abfe641294bfa38984c
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-02-18 18:12:55 +0200

    Removed connect-timeout and set timeouts manually

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import { port } from './utils/environment';
 import app from './app';
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+server.keepAliveTimeout = 120000; // 120 seconds
+server.headersTimeout = 120000; // 120 seconds
