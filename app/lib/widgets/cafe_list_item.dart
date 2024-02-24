c9c32955fcbe6cd50985698be9eea89a6db37122
Tuukka Viitanen <tuukka.viitanen@insolution.fi>
2024-02-24 09:51:01 +0200

    Added more information to cafe list

--- app/lib/widgets/cafe_list_item.dart
+++ app/lib/widgets/cafe_list_item.dart
@@ -1,5 +1,7 @@
 import 'package:cafe_now_app/models/place.dart';
+import 'package:cafe_now_app/screens/cafe_details_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CafeListItem extends StatelessWidget {
   const CafeListItem({
@@ -26,13 +28,40 @@ class CafeListItem extends StatelessWidget {
           ),
         ],
         color: selectedCafe?.place_id == cafe.place_id
-            ? Theme.of(context).colorScheme.primary
+            ? Theme.of(context).colorScheme.secondary
             : Colors.white,
         borderRadius: BorderRadius.circular(10),
       ),
       child: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Text(cafe.name, style: const TextStyle(fontSize: 25)),
+        child: ListTile(
+            title: Row(
+              children: [
+                Text(cafe.name, style: const TextStyle(fontSize: 25)),
+                const SizedBox(width: 20),
+                Text(
+                  cafe.opening_hours?.open_now == true
+                      ? 'Open'
+                      : cafe.opening_hours?.open_now == false
+                          ? 'Closed'
+                          : 'Unknown opening hours',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: cafe.opening_hours?.open_now == true
+                        ? Colors.green
+                        : cafe.opening_hours?.open_now == false
+                            ? Colors.red
+                            : Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            subtitle:
+                Text(cafe.vicinity ?? '', style: const TextStyle(fontSize: 20)),
+            trailing: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Details'),
+            )),
       ),
     );
   }
