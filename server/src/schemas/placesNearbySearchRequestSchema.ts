ec4b9c469d522fa1155e558d1ccee6efc2514f0a
Tuukka Viitanen <tuukka.viitanen@insolution.fi>
2024-02-24 18:24:57 +0200

    Changed backend to fetch cafes in order of proximity
    Radius is no longer supported as it will find just the 20 nearest cafes

--- server/src/schemas/placesNearbySearchRequestSchema.ts
+++ server/src/schemas/placesNearbySearchRequestSchema.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 export const placesNearbySearchRequestSchema = z.object({
   latitude: z.coerce.number().min(-90).max(90),
   longitude: z.coerce.number().min(-180).max(180),
-  radius: z.coerce.number().positive(),
 });
 
 export type PlacesNearbySearchRequest = z.infer<
