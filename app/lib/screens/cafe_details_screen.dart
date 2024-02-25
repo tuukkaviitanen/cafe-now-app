d2fefec462720ee981a28a1abc6df846481bf96b
Tuukka Viitanen <tuukka.viitanen@insolution.fi>
2024-02-25 11:57:13 +0200

    Added conditional rendering for ratings
    Also text aligned Open in Maps -button

--- app/lib/screens/cafe_details_screen.dart
+++ app/lib/screens/cafe_details_screen.dart
@@ -18,78 +18,84 @@ class CafeDetailsScreen extends StatelessWidget {
           title: Text(cafe.name),
           backgroundColor: Theme.of(context).colorScheme.primary,
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Hero(
-                    tag: cafe.place_id,
-                    child: Image.asset(
-                        'assets/images/CuteCoffeeMugNoBackground.png')),
-              ),
-              Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Card(
-                    color: Colors.white,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Column(
-                            children: [
-                              Text(cafe.name,
-                                  textAlign: TextAlign.center,
-                                  style:
-                                      Theme.of(context).textTheme.displayLarge),
-                              const SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    '${cafe.rating} / 5',
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Hero(
+                  tag: cafe.place_id,
+                  child: Image.asset(
+                      'assets/images/CuteCoffeeMugNoBackground.png')),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Card(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text(cafe.name,
+                                textAlign: TextAlign.center,
+                                style:
+                                    Theme.of(context).textTheme.displayLarge),
+                            const SizedBox(height: 10),
+                            (cafe.rating != null &&
+                                    cafe.user_ratings_total != null)
+                                ? Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        '${cafe.rating} / 5',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .displayMedium,
+                                      ),
+                                      const Icon(Icons.star,
+                                          color: Colors.amber),
+                                      Text('(${cafe.user_ratings_total})'),
+                                    ],
+                                  )
+                                : Text('No ratings yet!',
                                     style: Theme.of(context)
                                         .textTheme
-                                        .displayMedium,
-                                  ),
-                                  const Icon(Icons.star, color: Colors.amber),
-                                  Text('(${cafe.user_ratings_total})'),
-                                ],
-                              ),
-                              const SizedBox(height: 10),
-                              Text(
-                                  textAlign: TextAlign.center,
-                                  cafe.vicinity ?? cafe.formatted_address ?? '',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .displayMedium),
-                              const SizedBox(height: 10),
-                              OpeningHours(cafe: cafe),
-                            ],
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              MapsLauncher.launchQuery(cafe.name);
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(20),
-                              child: Text(
-                                'Open in Maps',
-                                style: Theme.of(context).textTheme.displayLarge,
-                              ),
+                                        .displayMedium),
+                            const SizedBox(height: 10),
+                            Text(
+                                textAlign: TextAlign.center,
+                                cafe.vicinity ?? cafe.formatted_address ?? '',
+                                style:
+                                    Theme.of(context).textTheme.displayMedium),
+                            const SizedBox(height: 10),
+                            OpeningHours(cafe: cafe),
+                          ],
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            MapsLauncher.launchQuery(cafe.name);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Text(
+                              textAlign: TextAlign.center,
+                              'Open in Maps',
+                              style: Theme.of(context).textTheme.displayLarge,
                             ),
-                          )
-                        ],
-                      ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ));
   }
 }
