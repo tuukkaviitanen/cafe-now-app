eb093535d84db14f57454c1d663329e92589963f
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-11-24 21:28:06 +0200

    Fixed web issues

--- app/lib/services/location_service.dart
+++ app/lib/services/location_service.dart
@@ -1,13 +1,7 @@
 import 'package:geolocator/geolocator.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class LocationService {
   Future<Position> getLocation() async {
-    if (await Permission.location.isGranted) {
-      return Geolocator.getCurrentPosition();
-    } else {
-      await Permission.location.request();
-      return getLocation();
-    }
+    return await Geolocator.getCurrentPosition();
   }
 }
